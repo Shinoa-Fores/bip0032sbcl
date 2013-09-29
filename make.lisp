4974ab6e5984b8471f8f76a55cf24593b1a7504e
Debian Live user <amnesia@localhost.localdomain>
2013-09-29 19:05:53 +0000

    Added make.lisp to compile the files and build the executable

---
+++ make.lisp
@@ -0,0 +1,8 @@
+(compile-file "config.lisp")
+(compile-file "secp256k1.lisp")
+(compile-file "hdw.lisp")
+(compile-file "bip0032sbcl.lisp")
+(load "config")
+(load "secp256k1")
+(load "hdw")
+(load "bip0032sbcl")
